e0bc5933ee0909f11a36500763063130622dc73a
Alejandro Jesus Oropesa Hernandez <159271629+Alejandro0729@users.noreply.github.com>
2024-08-22 01:18:00 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,16 +1,16 @@
 // Create a web server
 // Create a route that sends a response to the client
 
-const express = require('express');
-const server = express();
-const port = 3000;
+// const express = require('express');
+// const server = express();
+// const port = 3000;
 
-server.get('/comments', (req, res) => {
-  res.send('These are the comments');
-});
+// server.get('/comments', (req, res) => {
+//   res.send('These are the comments');
+// });
 
-server.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
+// server.listen(port, () => {
+//   console.log(`Server is listening on port ${port}`);
+// });
 
 // Run the server with node comments.js and visit http://localhost:3000/comments in the browser to see the response
